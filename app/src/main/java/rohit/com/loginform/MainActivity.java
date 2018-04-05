2e46cba8f8a952a0fa18ec7254512129b2645955
rohit9075 <rohit9075@gmail.com>
2018-04-05 23:16:35 +0530

    Initial commit

---
+++ app/src/main/java/rohit/com/loginform/MainActivity.java
@@ -0,0 +1,25 @@
+package rohit.com.loginform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mLoginButton = findViewById(R.id.button);
+        mLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (R.id.button == v.getId()){
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show();
+        }
+    }
+}
